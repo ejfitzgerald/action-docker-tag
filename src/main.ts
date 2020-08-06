ce7588588220920db6e62b317231b3542b6dad17
Ed FitzGerald <ejafitzgerald@gmail.com>
2020-08-06 22:24:35 +0100

    Update actions settings for initial testing

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,62 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 import {exec as execOrig} from 'child_process'
 import {promisify} from 'util'
+import {generateTagListFromVersion} from './tags'
 
-const exec = promisify(execOrig);
+const exec = promisify(execOrig)
 
 async function determineVersion(): Promise<string> {
-  const result = await exec('git describe --tags --always --dirty=-wip')
-  console.log(result)
+  const {stdout} = await exec('git describe --tags --always --dirty=-wip')
+  return stdout.trim()
+}
 
-  return ''
+async function buildDockerImage(
+  dockerfile: string,
+  context: string,
+  repo: string,
+  tag: string,
+): Promise<void> {
+  await exec(`docker build -f "${dockerfile}" -t ${repo}:${tag} "${context}"`)
+  return
+}
+
+async function tagDockerImage(
+  repo: string,
+  orig_tag: string,
+  new_tag: string,
+): Promise<void> {
+  const original = `${repo}:${orig_tag}`
+  const destination = `${repo}:${new_tag}`
+
+  await exec(`docker tag ${original} ${destination}`)
+  return
 }
 
 async function run(): Promise<void> {
   try {
-    const [err, stdout, stderr] = await determineVersion()
-    core.debug(`Version: ${stdout}`)
-    // const ms: string = core.getInput('milliseconds')
+
+    // determine the version for the project
+    const projectVersion = await determineVersion()
+    core.debug(`Detected Version: ${projectVersion}`)
+
+    // generate the set of targets for the build
+    const tags = generateTagListFromVersion(projectVersion)
+    if (tags.length === 0) {
+      core.setFailed('Unable to generate tags for this revision')
+      // return
+    }
+
+    // build the docker image
+    const repo: string = core.getInput('repo', {required: true})
+    const dockerfile: string = core.getInput('dockerfile') || 'Dockerfile'
+    const context: string = core.getInput('context') || '.'
+
+    core.debug(`dockerfile = ${dockerfile}`)
+    core.debug(`context = ${context}`)
+
+    // build the docker image
+    await buildDockerImage(dockerfile, context, repo, tags[0])
+
     // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     //
     // core.debug(new Date().toTimeString())
